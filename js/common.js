e69ea7ce9730866be05b8ccaed9724c0c1a51197
Feyli <mathiscom64@gmail.com>
2023-02-09 00:15:05 +0100

    fixed cookies and added konami code easter egg

--- js/common.js
+++ js/common.js
@@ -1,8 +1,42 @@
-const common = fetch('../components/topnav.html');
-common.then(response => response.text())
+const topnav = fetch('../components/topnav.html');
+const cookies = fetch('../components/cookies.html');
+
+topnav.then(response => response.text())
     .then(data => {
         document.body.innerHTML = data + document.body.innerHTML;
         let activePageButton = document.getElementById(window.location.pathname.split('/').pop().split('.')[0])
         activePageButton.classList.add('active');
         activePageButton.removeAttribute('href');
     });
+cookies.then(response => response.text())
+    .then(data => {
+        document.body.innerHTML = document.body.innerHTML + data;
+    });
+
+window.onload = () => {
+    if (!localStorage.getItem('cookies')) {
+        document.getElementById('cookie-banner').style.display = 'block';
+        document.getElementById('cookie-banner-accept').addEventListener('click', () => {
+            document.getElementById('cookie-banner').style.display = 'none';
+            localStorage.setItem('cookies', 'true');
+        });
+    }
+};
+
+function onKonamiCode(cb) {
+    let input = '';
+    const key = 'ArrowUpArrowUpArrowDownArrowDownArrowLeftArrowRightArrowLeftArrowRightKeyBKeyQ';
+    document.addEventListener('keydown', e => {
+        input += ("" + e.code);
+        if (input === key) {
+            return cb();
+        }
+        if (!key.indexOf(input)) return;
+        input = ("" + e.code);
+    });
+}
+
+onKonamiCode(() => {
+    let newTab = window.open();
+    newTab.location.href = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ';
+});
--- js/page1.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    if (window.localStorage.getItem("cookie") !== "true") {
-        document.getElementById("cookie-banner").style.display = "block";
-        document.getElementById("cookie-banner-accept").addEventListener("click", function () {
-            document.getElementById("cookie-banner").style.display = "none";
-            window.localStorage.setItem("cookie", "true");
-        });
-    }
-});
