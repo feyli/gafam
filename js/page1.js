5d1209efdefdea0c44404c77afa9d89e248fa5b7
Adrien Elslander <elslanderadrien@gmail.com>
2023-02-08 18:48:17 +0100

    Resolved some minor problems.

--- js/page1.js
+++ js/page1.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     if (window.localStorage.getItem("cookie") !== "true") {
         document.getElementById("cookie-banner").style.display = "block";
         document.getElementById("cookie-banner-accept").addEventListener("click", function () {
